7d9e240069b836e0962c5ad10a5fd5a4ee61cbad
gw3000 <weissenbaeck@posteo.de>
2023-07-08 22:19:10 +0200

    Zustand Volieren #16

--- aviary/models.py
+++ aviary/models.py
@@ -3,13 +3,14 @@ from uuid import uuid4
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+AVIARY_CHOICE = [("Offen","Offen"), ("Geschlossen", "Geschlossen"), ("Gesperrt", "Geshlossem")]
 
 class Aviary(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     description = models.CharField(
         max_length=256, verbose_name=_("Beschreibung"), unique=True
     )
-    condition = models.CharField(max_length=256, verbose_name=_("Zustand"))
+    condition = models.CharField(max_length=256, choices=AVIARY_CHOICE, verbose_name=_("Zustand"))
     last_ward_round = models.DateField(verbose_name=_("letzte Visite"))
 
     class Meta:
