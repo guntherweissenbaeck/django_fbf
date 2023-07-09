49c5a0fb98a7aaa202f646902309a19a5a93b6c4
gw3000 <weissenbaeck@posteo.de>
2023-07-09 22:48:33 +0200

    Zustand Volieren #16

--- aviary/models.py
+++ aviary/models.py
@@ -3,14 +3,14 @@ from uuid import uuid4
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-AVIARY_CHOICE = [("Offen","Offen"), ("Geschlossen", "Geschlossen"), ("Gesperrt", "Gesperrt")]
+CHOICE_AVIARY = [("Offen","Offen"), ("Geschlossen", "Geschlossen"), ("Gesperrt", "Gesperrt")]
 
 class Aviary(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     description = models.CharField(
         max_length=256, verbose_name=_("Beschreibung"), unique=True
     )
-    condition = models.CharField(max_length=256, choices=AVIARY_CHOICE, verbose_name=_("Zustand"))
+    condition = models.CharField(max_length=256, choices=CHOICE_AVIARY, verbose_name=_("Zustand"))
     last_ward_round = models.DateField(verbose_name=_("letzte Visite"))
 
     class Meta:
--- aviary/views.py
+++ aviary/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Aviary
+from .forms import AviaryEditForm
 
 
 @login_required(login_url="account_login")
@@ -14,5 +15,11 @@ def aviary_all(request):
 @login_required(login_url="account_login")
 def aviary_single(request, id):
     aviary = Aviary.objects.get(id=id)
-    context = {"aviary": aviary}
+    form = AviaryEditForm(request.POST or None, instance=aviary)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("aviary_all")
+
+    context = {"aviary": aviary, "form": form}
     return render(request, "aviary/aviary_single.html", context)
