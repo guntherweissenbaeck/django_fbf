75765d85db55eadde62f653f492b68bb53ae2661
gw3000 <weissenbaeck@posteo.de>
2023-07-12 11:35:25 +0200

    postgres db

--- app/core/settings.py
+++ app/core/settings.py
@@ -106,8 +106,14 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_traefik",
+        "USER": "django_traefik",
+        "PASSWORD": "django_traefik",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
