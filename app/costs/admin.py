395032091520beaf711d061ebe22c813a5f00239
gw3000 <weissenbaeck@posteo.de>
2023-07-14 19:16:30 +0200

    Flake8 Errors corrected

--- app/costs/admin.py
+++ app/costs/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Costs
 
+
 @admin.register(Costs)
 class FallenBirdAdmin(admin.ModelAdmin):
     list_display = [
@@ -10,5 +11,5 @@ class FallenBirdAdmin(admin.ModelAdmin):
         "created",
         "comment",
         "user",
-        ]
+    ]
     list_filter = ("id_bird", "created")
