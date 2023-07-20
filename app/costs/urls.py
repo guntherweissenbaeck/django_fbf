5579f1a4dfdcda0e926ba93f059305a8790fbf5a
gw3000 <weissenbaeck@posteo.de>
2023-07-20 11:40:50 +0200

    individual costs url

--- app/costs/urls.py
+++ app/costs/urls.py
@@ -10,6 +10,7 @@ from .views import (
 urlpatterns = [
     path("all/", costs_all, name="costs_all"),
     path("create/", costs_create, name="costs_create"),
+    path("create/<id>", costs_create, name="costs_create"),
     path("edit/<id>", costs_edit, name="costs_edit"),
     path("delete/<id>", costs_delete, name="costs_delete"),
 ]
