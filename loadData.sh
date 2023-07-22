9d81af7c55b74d1ec06e363c7d5bf5db96f1f206
gw3000 <weissenbaeck@posteo.de>
2023-07-22 22:20:16 +0200

    script for loading initial Data

---
+++ loadData.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+docker-compose exec web python manage.py loaddata fixtures/defaultUser
+docker-compose exec web python manage.py loaddata fixtures/bird
+docker-compose exec web python manage.py loaddata fixtures/birdCircumstance
+docker-compose exec web python manage.py loaddata fixtures/birdStatus
