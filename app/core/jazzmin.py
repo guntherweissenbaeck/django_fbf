7e151e937375579464243dbcf6b2f6ec9cdd8d4a
gw3000 <weissenbaeck@posteo.de>
2023-07-23 12:24:35 +0200

    logos and pics

--- app/core/jazzmin.py
+++ app/core/jazzmin.py
@@ -6,13 +6,13 @@ JAZZMIN_SETTINGS = {
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "Fallen Birdy Form",
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "img/logo.png",
+    "site_logo": "img/admin/m.png",
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": "img/logo.png",
+    "login_logo": "img/admin/m.png",
     # Logo to use for login form in dark themes (defaults to login_logo)
     # "login_logo_dark": None,
     # CSS classes that are applied to the logo above
-    "site_logo_classes": "img-circle",
+    # "site_logo_classes": "img-circle",
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": None,
     # Welcome text on the login screen
