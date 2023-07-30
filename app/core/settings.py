9e01650cbfebe8bc7119550126d68320b53bef36
gw3000 <weissenbaeck@posteo.de>
2023-07-30 19:25:49 +0200

    Email Backend

--- app/core/settings.py
+++ app/core/settings.py
@@ -213,9 +213,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 3
 ACCOUNT_UNIQUE_EMAIL = True
 LOGIN_REDIRECT_URL = "/bird/all"
 
-# Email
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+# CKEditor
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "media"
 
@@ -231,3 +229,19 @@ try:
     from .jazzmin import JAZZMIN_SETTINGS
 except ImportError:
     print("No Jazzmin Settings found!")
+
+# Email
+
+# Console Backend for Development Usage.
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# SMTP Backup for Production Usage.
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+if EMAIL_BACKEND == "django.core.mail.backends.smtp.EmailBackend":
+    DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    EMAIL_HOST = env("EMAIL_HOST")
+    EMAIL_PORT = env("EMAIL_PORT")
+    EMAIL_USE_TLS = True
