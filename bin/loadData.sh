c0d125809bfeda3865a97741205c091972f8bdd3
gw3000 <weissenbaeck@posteo.de>
2023-07-30 13:35:41 +0200

    renamed file

--- bin/loadData.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-# docker-compose exec web python manage.py loaddata fixtures/defaultUser
-# docker-compose exec web python manage.py loaddata fixtures/bird
-# docker-compose exec web python manage.py loaddata fixtures/birdCircumstance
-# docker-compose exec web python manage.py loaddata fixtures/birdStatus
-
-docker-compose exec web python manage.py loaddata fixtures/{bird.json,birdCircumstance,birdStatus.json,defaultUser.json}
