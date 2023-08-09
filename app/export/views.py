b346ed954a8cb56b98bf984313ffa28c5cfa53e1
gw3000 <weissenbaeck@posteo.de>
2023-08-09 22:31:47 +0200

    CSV Export Fixes #2 little icons

--- app/export/views.py
+++ app/export/views.py
@@ -1,10 +1,14 @@
 import csv
+from datetime import date
 
 from bird.models import FallenBird
 from costs.models import Costs
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+
+
+today = date.today().strftime("%Y-%m-%d")
 
 
 @login_required(login_url="account_login")
@@ -15,10 +19,13 @@ def site_exports(request):
 @login_required(login_url="account_login")
 def export_costs(request):
     costs = Costs.objects.all().values_list(
-        "id_bird__bird_identifier", "costs", "created", "comment", "user__username"
-    )
+        "id_bird__bird_identifier",
+        "costs",
+        "created",
+        "comment",
+        "user__username")
     response = HttpResponse(content_type="text/csv")
-    response["Content-Disposition"] = "attachment, filename=fbf_costs.csv"
+    response["Content-Disposition"] = f"attachment, filename=fbf_costs_{today}.csv"
     writer = csv.writer(response)
     writer.writerow(
         ["Vogel", "Betrag in Euro", "Gebucht am", "Kommentar", "Gebucht von"]
@@ -48,7 +55,7 @@ def export_birds(request):
         "sent_to",
     )
     response = HttpResponse(content_type="text/csv")
-    response["Content-Disposition"] = "attachment, filename=fbf_birds.csv"
+    response["Content-Disposition"] = f"attachment, filename=fbf_birds_{today}.csv"
     writer = csv.writer(response)
     writer.writerow(
         [
