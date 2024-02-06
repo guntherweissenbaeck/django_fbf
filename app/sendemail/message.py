9f40e63981b4d9a2eb5308d162637902b58320d7
Gunther Weißenbäck <98875505+guntherweissenbaeck@users.noreply.github.com>
2024-02-06 10:53:14 +0100

    Update message.py

--- app/sendemail/message.py
+++ app/sendemail/message.py
@@ -1,18 +1,18 @@
 def messagebody(date, bird, place, diagnosis,) -> str:
     """Returns the body of the message to be sent to UNB."""
     body = f"""
-        Sehr geehrte Damen und Herren,
+Sehr geehrte Damen und Herren,
 
-        am {date} wurde in der NABU Wildvogelhilfe ein Vogel der Art {bird} aufgenomen.
-        Der Fundort laut Finder*in war: {place}.
-        Die Diagnose bei Fund lautet: {diagnosis}.
+am {date} wurde in der NABU Wildvogelhilfe ein Vogel der Art {bird} aufgenomen.
+Der Fundort laut Finder*in war: {place}.
+Die Diagnose bei Fund lautet: {diagnosis}.
 
-        Mit freundlichen Grüßen
+Mit freundlichen Grüßen
 
-        NABU Wildvogelhilfe Jena
-        Untergliederung des
-        NABU Kreisverband Jena e.V.
-        Schillergässchen 5
-        07745 Jena
-        """
+NABU Wildvogelhilfe Jena
+Untergliederung des
+NABU Kreisverband Jena e.V.
+Schillergässchen 5
+07745 Jena
+"""
     return body
