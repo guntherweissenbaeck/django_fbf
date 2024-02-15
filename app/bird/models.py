37df7cdab0c16d92d98bc11e3876b2d10ab58e1b
Gunther Weissenbaeck <weissenbaeck@posteo.de>
2024-02-15 11:03:31 +0100

    Automatische Meldung an UNB Fixes #26

--- app/bird/models.py
+++ app/bird/models.py
@@ -32,45 +32,22 @@ def costs_default():
 
 class FallenBird(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    bird_identifier = models.CharField(
-        max_length=256, verbose_name=_("Patienten Alias")
-    )
+    bird_identifier = models.CharField( max_length=256, verbose_name=_("Patienten Alias"))
     bird = models.ForeignKey("Bird", on_delete=models.CASCADE, verbose_name=_("Vogel"))
     age = models.CharField(max_length=15, choices=CHOICE_AGE, verbose_name=_("Alter"))
-    sex = models.CharField(
-        max_length=15, choices=CHOICE_SEX, verbose_name=_("Geschlecht")
-    )
+    sex = models.CharField( max_length=15, choices=CHOICE_SEX, verbose_name=_("Geschlecht"))
     date_found = models.DateField(verbose_name=_("Datum des Fundes"))
     place = models.CharField(max_length=256, verbose_name=_("Ort des Fundes"))
     created = models.DateTimeField(auto_now_add=True, verbose_name=_("angelegt am"))
     updated = models.DateTimeField(auto_now=True, verbose_name=_("geändert am"))
-    find_circumstances = models.ForeignKey(
-        "Circumstance", on_delete=models.CASCADE, verbose_name=_("Fundumstände")
-    )
-    diagnostic_finding = models.CharField(
-        max_length=256, verbose_name=_("Diagnose bei Fund")
-    )
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_("Benutzer")
-    )
+    find_circumstances = models.ForeignKey( "Circumstance", on_delete=models.CASCADE, verbose_name=_("Fundumstände"))
+    diagnostic_finding = models.CharField( max_length=256, verbose_name=_("Diagnose bei Fund"))
+    user = models.ForeignKey( settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_("Benutzer"))
     status = models.ForeignKey("BirdStatus", on_delete=models.CASCADE, default=1)
-    aviary = models.ForeignKey(
-        Aviary,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
-        verbose_name=_("Voliere"),
-    )
-    sent_to = models.CharField(
-        max_length=256, null=True, blank=True, verbose_name=_("Übersandt nach")
-    )
+    aviary = models.ForeignKey( Aviary, on_delete=models.SET_NULL, blank=True, null=True, verbose_name=_("Voliere"),)
+    sent_to = models.CharField( max_length=256, null=True, blank=True, verbose_name=_("Übersandt nach"))
     comment = models.TextField(blank=True, null=True, verbose_name=_("Bemerkung"))
-    finder = models.TextField(
-        blank=True,
-        null=True,
-        verbose_name=_("Finder"),
-        default="Vorname: \nNachname: \nStraße: \nHausnummer: \nStadt: \nPLZ: \nTelefonnummer: ",
-    )
+    finder = models.TextField( blank=True, null=True, verbose_name=_("Finder"), default="Vorname: \nNachname: \nStraße: \nHausnummer: \nStadt: \nPLZ: \nTelefonnummer: ",)
 
     class Meta:
         verbose_name = _("Patient")
