cc16f0ad3ac98d06df7f0d45592c6b5429eaf85b
Gunther Weissenbaeck <weissenbaeck@posteo.de>
2024-05-21 22:44:24 +0200

    wrote some tests

--- app/bird/tests.py
+++ app/bird/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class BirdTestCase(TestCase):
+    def setUp(self):
+        Bird.objects.create(
+            name="Vogel 1",
+            species="Art 1",
+            aviary=Aviary.objects.create(
+                description="Voliere 1",
+                condition="Offen",
+                last_ward_round="2021-01-01",
+                comment="Test",
+            ),
+            date_of_birth="2020-01-01
--- app/costs/tests.py
+++ app/costs/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Write costs tests here
+class AviaryTestCase(TestCase):
+    def setUp(self):
+        Aviary.objects.create(
+            description="Voliere 1",
+            condition="Offen",
+            last_ward_round="2021-01-01",
+            comment="Test",
+        )
+
+    def test_aviary_description(self):
+        aviary = Aviary.objects.get(description="Voliere 1")
+        self.assertEqual(aviary.description, "Voliere 1")
+
+    def test_aviary_condition(self):
+        aviary = Aviary.objects.get(description="Voliere 1")
+        self.assertEqual(aviary.condition, "Offen")
+
+    def test_aviary_last_ward_round(self):
+        aviary = Aviary.objects.get(description="Voliere 1")
+        self.assertEqual(aviary.last_ward_round, "2021-01-01")
+
+    def test_aviary_comment(self):
+        aviary = Aviary.objects.get(description="Voliere 1")
+        self.assertEqual(aviary.comment, "Test")
