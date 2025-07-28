e99cdce5cdfbe10a18bb2ebbd0f4656a96b8b91b
NABU Jena <post@nabu-jena.de>
2025-07-28 20:47:35 +0200

    expand manuel export

--- app/export/urls.py
+++ app/export/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from .views import (
     export_birds,
-    export_costs,
+    export_birds_all, 
+    export_birds_custom,
     site_exports,
 )
 
 urlpatterns = [
     path("", site_exports, name="site_exports"),
-    path("birds/", export_birds, name="export_birds"),
-    path("costs/", export_costs, name="export_costs"),
+    path("birds/", export_birds, name="export_birds"),  # Legacy compatibility
+    path("birds/all/", export_birds_all, name="export_birds_all"),
+    path("birds/custom/", export_birds_custom, name="export_birds_custom"),
 ]
